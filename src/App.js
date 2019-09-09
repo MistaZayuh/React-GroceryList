89cf6081a446c2a03d692fd16f3d13bc86769c29
Isaiah Boren <MistaZayuh@gmail.com>
2019-09-09 15:22:30 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = { 
+    grocs: [
+      {id: 1, name: "Milk", bought: true },
+      {id: 2, name: "Bread", bought: false },
+      {id: 3, name: "Egg", bought: true },
+    ]
+  };
+
+  renderGrocs = () => {
+    const { grocs, } = this.state
+    return grocs.map( groc => 
+      <li key={ groc.id }>{ groc.name }</li> 
+    );
+  };
+  render() {
+    return(
+      <div>
+        <ul>
+          { this.renderGrocs() }
+        </ul>
+      </div>
+    );
+  };
+};
 
 export default App;
---
+++ src/List.js
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "./App";
+
+class List extends React.Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  };
+};
